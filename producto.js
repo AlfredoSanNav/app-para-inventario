2b336ad17705691534c25d35f454def7bdcf4be3
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-10-26 20:30:28 -0600

    Agrega la clase Producto

---
+++ producto.js
@@ -0,0 +1,10 @@
+export default class Producto{
+    constructor(codigo, nombre, descripcion, costo, cantidad){
+        this.codigo = codigo
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.costo = costo 
+        this.cantidad = cantidad
+    }
+    
+}
